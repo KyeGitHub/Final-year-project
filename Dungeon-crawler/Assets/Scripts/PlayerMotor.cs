05664ab988d8b5bf0fac9ecb60513313e907909f
KyeGitHub <Kyehammond97@gmail.com>
2019-10-23 20:44:58 +0100

    Added in stats

--- Dungeon-crawler/Assets/Scripts/PlayerMotor.cs
+++ Dungeon-crawler/Assets/Scripts/PlayerMotor.cs
@@ -47,6 +47,6 @@ public class PlayerMotor : MonoBehaviour
     {
         Vector3 direction = (target.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
-        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 10f);
     }
 }
---
+++ Dungeon-crawler/Assets/Scripts/Stats/PlayerStats.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats
+{
+   void Start()
+    {
+        EquipmentManager.instance.onEquipmentChanged += onEquipmentChanged;
+    }
+
+    void onEquipmentChanged(Equipment newItem, Equipment oldItem)
+    {
+        if (newItem != null)
+        {
+            armour.AddModifier(newItem.armourModifier);
+            damage.AddModifier(newItem.damageModifier);
+        }
+        
+        if (oldItem != null)
+        {
+            armour.RemoveModifier(oldItem.armourModifier);
+            damage.RemoveModifier(oldItem.damageModifier);
+        }
+    }
+
+}
