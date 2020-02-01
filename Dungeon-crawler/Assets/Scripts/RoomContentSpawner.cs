3897ac47258d083da7191a1efee05194559b9049
KyeGitHub <Kyehammond97@gmail.com>
2020-02-01 19:43:54 +0000

    spawn support

--- Dungeon-crawler/Assets/Scripts/RoomContentSpawner.cs
+++ Dungeon-crawler/Assets/Scripts/RoomContentSpawner.cs
@@ -4,17 +4,45 @@ using UnityEngine;
 
 public class RoomContentSpawner : MonoBehaviour
 {
-    public GameObject[] spawnPoints;
+    public string searchTag;
+    public List<GameObject> spawnList = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (searchTag != null)
+        {
+            FindObjectwithTag(searchTag);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+    }
+
+    public void FindObjectwithTag(string _tag)
+    {
+        spawnList.Clear();
+        Transform parent = transform;
+        GetChildObject(parent, _tag);
+    }
+
+    public void GetChildObject(Transform parent, string _tag)
+    {
+        for (int i = 0; i < parent.childCount; i++)
+        {
+            Transform child = parent.GetChild(i);
+            if (child.tag == _tag)
+            {
+                spawnList.Add(child.gameObject);
+            }
+            if (child.childCount > 0)
+            {
+                GetChildObject(child, _tag);
+            }
+        }
     }
 }
