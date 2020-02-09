790655d2aff8d1eb49c1c5360f0d09c68fd3b6a7
KyeGitHub <Kyehammond97@gmail.com>
2020-02-09 21:52:04 +0000

    More spawn support
    Added in more framework for content spawning

--- Dungeon-crawler/Assets/Scripts/RoomContentSpawner.cs
+++ Dungeon-crawler/Assets/Scripts/RoomContentSpawner.cs
@@ -5,15 +5,55 @@ using UnityEngine;
 public class RoomContentSpawner : MonoBehaviour
 {
     public string searchTag;
+    public string roomTag;
     public List<GameObject> spawnList = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
+        roomTag = transform.tag; // grab the room tag which was assigned by the spawner, use this to determine what we're putting in the room
+
+        // checks all children for specified tag as unity searches whole scene instead of just children
         if (searchTag != null)
         {
             FindObjectwithTag(searchTag);
         }
+       
+        if (roomTag == "Boss Room")
+        {
+
+        }
+
+        else if (roomTag == "Combat Room")
+        {
+
+        }
+
+        else if (roomTag == "Resource Room")
+        {
+
+        }
+
+        else if (roomTag == "Spawn Room")
+        {
+
+        }
+
+        else if (roomTag == "Demi Boss Room")
+        {
+
+        }
+
+        else if (roomTag == "Mixed Room")
+        {
+
+        }
+
+        else
+        {
+            Debug.Log("Something messed up with your room tag for content spawning");
+        }
+
     }
 
     // Update is called once per frame
@@ -23,8 +63,56 @@ public class RoomContentSpawner : MonoBehaviour
 
     }
 
+    #region Spawning
+    // Depending on room tag these will be called in the start() function
+    void CombatContentSpawner()
+    {
+        
+        
+
+    }
+    void MixedContentSpawner()
+    {
+
+
+
+    }
+
+    void DemiContentSpawner()
+    {
+
+
+
+    }
+
+    void ResourceContentSpawner()
+    {
+
+
+
+    }
+
+    void BossContentSpawner()
+    {
+        
+        
+
+    }
+
+    void SpawnContentSpawner()
+    {
+
+
+
+    }
+
+    #endregion
+
+    #region Tag finder
+
     public void FindObjectwithTag(string _tag)
     {
+        // clears list, sets what object we want to search then calls the function to check for tags
         spawnList.Clear();
         Transform parent = transform;
         GetChildObject(parent, _tag);
@@ -32,6 +120,7 @@ public class RoomContentSpawner : MonoBehaviour
 
     public void GetChildObject(Transform parent, string _tag)
     {
+        // loops through all the child objects of the parent to find any with the tag we're looking for
         for (int i = 0; i < parent.childCount; i++)
         {
             Transform child = parent.GetChild(i);
@@ -45,4 +134,6 @@ public class RoomContentSpawner : MonoBehaviour
             }
         }
     }
+    #endregion
+
 }
--- Dungeon-crawler/Assets/Scripts/RoomSpawner.cs
+++ Dungeon-crawler/Assets/Scripts/RoomSpawner.cs
@@ -22,9 +22,10 @@ public class RoomSpawner : MonoBehaviour
     public int dungeonSize; // how many rows/collumns, e.g. 10 sets it to 10 by 10, 100 rooms.
 
     // a count of each type of room as we need to make sure there is a variety to make sure the floor is completable
-    public int combatRooms;
-    public int resourceRooms;
-    public int demibossRooms;
+    public int combatRooms = 0;
+    public int resourceRooms = 0;
+    public int demibossRooms = 0;
+    public int demibossRoomsMax = 4;
     public bool spawnRoomSpawned;
     public bool bossRoomSpawned;
 
@@ -55,6 +56,7 @@ public class RoomSpawner : MonoBehaviour
         int bossRoomSide = Random.Range(1, 4);
 
         Debug.Log("Side: " + bossRoomSide);
+
         for (int i = -bounds; i < bounds + 40; i += 40)
         {
             for (int j = -bounds; j < bounds + 40; j += 40)
@@ -64,57 +66,67 @@ public class RoomSpawner : MonoBehaviour
                     // Top right corner room
                     Debug.Log("Top right " + roomCount);
                     GameObject instance = Instantiate(topRightRoom, (gameObject.transform.position) + new Vector3(i, 0, j), Quaternion.identity) as GameObject;
-                    
+                    AssignRoomType(instance);
                 }
                 else if (j == -bounds & i == bounds)
                 {
                     // Bottom right corner room
                     Debug.Log("Bottom right " + roomCount);
                     GameObject instance = Instantiate(bottomRightRoom, (gameObject.transform.position) + new Vector3(i, 0, j), Quaternion.identity) as GameObject;
+                    AssignRoomType(instance);
                 }
                 else if (i == -bounds & j == -bounds)
                 {
                     // Bottom left corner room
                     Debug.Log("Bottom Left " + roomCount);
                     GameObject instance = Instantiate(bottomLeftRoom, (gameObject.transform.position) + new Vector3(i, 0, j), Quaternion.identity) as GameObject;
+                    AssignRoomType(instance);
                 }
                 else if (i == -bounds & j == bounds)
                 {
                     // Top left corner room
                     Debug.Log("Top Left " + roomCount);
                     GameObject instance = Instantiate(topLeftRoom, (gameObject.transform.position) + new Vector3(i, 0, j), Quaternion.identity) as GameObject;
+                    AssignRoomType(instance);
                 }
                 else if (i == -bounds)
                 {
                     // Edge room
                     GameObject instance = Instantiate(leftWall, (gameObject.transform.position) + new Vector3(i, 0, j), Quaternion.identity) as GameObject;
+                    AssignRoomType(instance);
                 }
                 else if (i == bounds)
                 {
                     // Edge room
                     GameObject instance = Instantiate(rightWall, (gameObject.transform.position) + new Vector3(i, 0, j), Quaternion.identity) as GameObject;
+                    AssignRoomType(instance);
                 }
                 else if (j == -bounds)
                 {
                     // Edge room
                     GameObject instance = Instantiate(bottomWall, (gameObject.transform.position) + new Vector3(i, 0, j), Quaternion.identity) as GameObject;
+                    AssignRoomType(instance);
+
                 }
                 else if (j == bounds)
                 {
                     // Edge room
                     GameObject instance = Instantiate(topWall, (gameObject.transform.position) + new Vector3(i, 0, j), Quaternion.identity) as GameObject;
+                    AssignRoomType(instance);
                 }
                 else if (j == 0 & i == 0)
                 {
                     // Spawn room
                     GameObject instance = Instantiate(spawnRoom, (gameObject.transform.position) + new Vector3(i, 0, j), Quaternion.identity) as GameObject;
                     instance.tag = "Spawn Room";
+                    spawnRoomSpawned = true;
                 }
                 else if (j==40 & i == 0 & bossRoomSide == 1)
                 {
                     Debug.Log("Boss room " + bossRoomSide);
                     GameObject instance = Instantiate(bossRoom, (gameObject.transform.position) + new Vector3(i, 0, j), Quaternion.identity) as GameObject;
                     instance.tag = "Boss Room";
+                    bossRoomSpawned = true;
                 }
                 else if (j == 0 & i == 40 & bossRoomSide == 2)
                 {
@@ -143,5 +155,67 @@ public class RoomSpawner : MonoBehaviour
                 roomCount++;
             }
         }
+    }
+
+void AssignRoomType(GameObject room)
+    {
+        if (demibossRooms < demibossRoomsMax) // if we have not reached the max cap of demiboss rooms
+        {
+            int randomNumber = Random.Range(0, 101);
+            Debug.Log(randomNumber);
+
+            if (randomNumber < 50) // demi boss rooms have more weight so that the dungeon SHOULD include at least 1, later could loop through rooms if no demi boss rooms were spawned but it is statistically unlikely
+            {
+                room.tag = "Demi Boss Room";
+                demibossRooms++;
+                Debug.Log(room.tag);
+            }
+
+            else if (randomNumber > 49 && randomNumber < 70)
+            {
+                room.tag = "Resource Room";
+                Debug.Log(room.tag);
+            }
+            else if (randomNumber > 69 && randomNumber < 90)
+            {
+                room.tag = "Combat Room";
+                Debug.Log(room.tag);
+            }
+            else if (randomNumber > 89)
+            {
+                room.tag = "Mixed Room";
+                Debug.Log(room.tag);
+            }
+            else
+            {
+                Debug.Log("Something went wrong in your room type assigner");
+            }
+        }
+
+        else // max amount of demiboss rooms, continue without checking for them
+        {
+            int randomNumber = Random.Range(0, 101);
+
+            if (randomNumber < 40)
+            {
+                room.tag = "Resource Room";
+                Debug.Log(room.tag);
+            }
+            else if (randomNumber > 39 && randomNumber < 80)
+            {
+                room.tag = "Combat Room";
+                Debug.Log(room.tag);
+            }
+            else if (randomNumber > 79)
+            {
+                room.tag = "Mixed Room";
+                Debug.Log(room.tag);
+            }
+            else
+            {
+                Debug.Log("Something went wrong in your room type assigner");
+            }
+        }
+        
     }
 }
