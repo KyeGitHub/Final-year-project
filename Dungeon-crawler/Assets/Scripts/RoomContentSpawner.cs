7838739f9af1d15e0e08757bd5ebc5f0f8e6678d
KyeGitHub <Kyehammond97@gmail.com>
2020-02-19 18:05:10 +0000

    Added spawning to everything but demiboss/boss/spawn room

--- Dungeon-crawler/Assets/Scripts/RoomContentSpawner.cs
+++ Dungeon-crawler/Assets/Scripts/RoomContentSpawner.cs
@@ -8,7 +8,7 @@ public class RoomContentSpawner : MonoBehaviour
     public string searchTag;
     public string roomTag;
     public List<GameObject> spawnList = new List<GameObject>();
-    public GameObject water, crystal;
+    public GameObject water, crystal, rangeEnemy, meleeEnemy, magicEnemy;
     public int nodeCount = 0;
 
     // Start is called before the first frame update
@@ -32,7 +32,10 @@ public class RoomContentSpawner : MonoBehaviour
 
         else if (roomTag == "Combat Room")
         {
-
+            foreach (GameObject spawn in spawnList)
+            {
+                CombatContentSpawner(spawn);
+            }
         }
 
         else if (roomTag == "Resource Room")
@@ -40,9 +43,9 @@ public class RoomContentSpawner : MonoBehaviour
             foreach (GameObject spawn in spawnList)
             {
                 ResourceContentSpawner(spawn);
-            }
-            
+            }           
         }
+
         else if (roomTag == "Spawn Room")
         {
 
@@ -55,7 +58,10 @@ public class RoomContentSpawner : MonoBehaviour
 
         else if (roomTag == "Mixed Room")
         {
-
+            foreach (GameObject spawn in spawnList)
+            {
+                MixedContentSpawner(spawn);
+            }
         }
 
         else
@@ -75,20 +81,120 @@ public class RoomContentSpawner : MonoBehaviour
 
     #region Spawning
     // Depending on room tag these will be called in the start() function
-    void CombatContentSpawner()
-    {
-        
-        
-
-    }
-    void MixedContentSpawner()
+    void CombatContentSpawner(GameObject spawn)
     {
 
+        int randomNumber = Random.Range(0, 101);
+        //Debug.Log(randomNumber +" " + spawn.name);
+        if (randomNumber < 76) // spawn something
+        {
+            int a = Random.Range(0, 101);
+            
+
+            if (a < 34)
+            {
+                Debug.Log("Melee spawned, a should be less than 34: " + a);
+                GameObject instance = Instantiate(meleeEnemy, spawn.transform.position + new Vector3(0, 0.01f, 0), Quaternion.identity) as GameObject;
+                instance.transform.parent = gameObject.transform;
+                nodeCount++;
+            }
+            
+            else if (a > 33 && a <67)
+            {
+                Debug.Log("Range spawned, a should be between 34 and 66: " + a);
+                GameObject instance = Instantiate(rangeEnemy, spawn.transform.position + new Vector3(0, 0.01f, 0), Quaternion.identity) as GameObject;
+                instance.transform.parent = gameObject.transform;
+                nodeCount++;
+            }
+            else if (a > 67)
+            {
+                Debug.Log("Magic spawned, a should be above 66: " + a);
+                GameObject instance = Instantiate(magicEnemy, spawn.transform.position + new Vector3(0, 0.01f, 0), Quaternion.identity) as GameObject;
+                instance.transform.parent = gameObject.transform;
+                nodeCount++;
+            }
+        }
+        
+        else if (randomNumber > 75 && nodeCount > 0) // if there's stuff in room don't have to spawn 
+        {
+            Debug.Log("Not spawning stuff, number should be above 75: " + randomNumber + " and this above 0: " + nodeCount);
+        }
+        else // go again
+        {
+            CombatContentSpawner(spawn);
+        }
+
+
+
+    }
+    void MixedContentSpawner(GameObject spawn)
+    {
+
+        int randomNumber = Random.Range(0, 101);
+        //Debug.Log(randomNumber +" " + spawn.name);
+        if (randomNumber < 41) // spawn enemy
+        {
+            int a = Random.Range(0, 101);
+
+
+            if (a < 34)
+            {
+                Debug.Log("Melee spawned, a should be less than 34: " + a);
+                GameObject instance = Instantiate(meleeEnemy, spawn.transform.position + new Vector3(0, 0.01f, 0), Quaternion.identity) as GameObject;
+                instance.transform.parent = gameObject.transform;
+                nodeCount++;
+            }
+
+            else if (a > 33 && a < 67)
+            {
+                Debug.Log("Range spawned, a should be between 34 and 66: " + a);
+                GameObject instance = Instantiate(rangeEnemy, spawn.transform.position + new Vector3(0, 0.01f, 0), Quaternion.identity) as GameObject;
+                instance.transform.parent = gameObject.transform;
+                nodeCount++;
+            }
+            else if (a > 67)
+            {
+                Debug.Log("Magic spawned, a should be above 66: " + a);
+                GameObject instance = Instantiate(magicEnemy, spawn.transform.position + new Vector3(0, 0.01f, 0), Quaternion.identity) as GameObject;
+                instance.transform.parent = gameObject.transform;
+                nodeCount++;
+            }
+        }
+
+        else if(randomNumber > 41 && randomNumber < 81) // spawn resource
+        {
+            int a = Random.Range(0, 101);
+
+            if (a < 50) // spawn water
+            {
+                Debug.Log(randomNumber + " spawning Water");
+                GameObject instance = Instantiate(water, spawn.transform.position + new Vector3(0, 0.01f, 0), Quaternion.identity) as GameObject;
+                instance.transform.parent = gameObject.transform;
+                nodeCount++;
+            }
+            else if (a > 51) // spawn crystal 
+            {
+                GameObject instance = Instantiate(crystal, spawn.transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity) as GameObject;
+                Debug.Log(randomNumber + " spawning Crystal");
+                instance.transform.Rotate(new Vector3(-90, 0, 0));
+                instance.transform.parent = gameObject.transform;
+                nodeCount++;
+            }
+        }
+
+        else if (randomNumber > 80 && nodeCount > 0) // if there's stuff in room don't have to spawn 
+        {
+            Debug.Log("Not spawning stuff, number should be above 75: " + randomNumber + " and this above 0: " + nodeCount);
+        }
+        else // go again
+        {
+            MixedContentSpawner(spawn);
+        }
 
 
     }
 
-    void DemiContentSpawner()
+    void DemiContentSpawner(GameObject spawn)
     {
 
 
@@ -129,14 +235,14 @@ public class RoomContentSpawner : MonoBehaviour
 
     }
 
-    void BossContentSpawner()
+    void BossContentSpawner(GameObject spawn)
     {
         
         
 
     }
 
-    void SpawnContentSpawner()
+    void SpawnContentSpawner(GameObject spawn)
     {
 
 
